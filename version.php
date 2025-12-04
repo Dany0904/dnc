85785d336f95753e67de05c8121b176d4fd30e27
Daniel Ortiz <albertodanielortiz04@gmail.com>
2025-12-04 10:25:30 -0600

    install.xml actualizado

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_dnc'; // Nombre interno.
-$plugin->version   = 2025120100; // YYYYMMDDHH (hora opcional).
+$plugin->version   = 2025012200; // YYYYMMDDHH (hora opcional).
 $plugin->requires  = 2022041900.00; // Requiere Moodle 4.0+.
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1 alpha';
